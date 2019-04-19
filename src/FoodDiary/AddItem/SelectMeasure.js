054b8b3b51fd30e685c528e2603911c6fc949aae
Nikhil Sharma (Mac) <n89sharma@gmail.com>
2019-04-19 18:05:04 -0400

    working commit 2

--- src/FoodDiary/AddItem/SelectMeasure.js
+++ src/FoodDiary/AddItem/SelectMeasure.js
@@ -7,13 +7,29 @@ import axios from 'axios';
 class SelectMeasure extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      measureOptions: [],
+      isLoading: false
+    };
     this.setMeasureOptions(this.props.foodId);
   }
 
   setMeasureOptions = (foodId) => {
-    console.log(`asdf ${foodId}`);
-    //axios
-    //  .get('http://localhost:8080/food')
+    axios
+     .get(`http://localhost:8080/food/${foodId}/measure`)
+     .then(response => {
+       this.setState({
+         measureOptions: response.data.map(measureData => measureData.measureName)
+        });
+     })
+     .catch(error => {
+       console.log(error);
+     })
+     .then(() => {
+       this.setState({
+         isLoading: false
+       });
+     })
   }
 
   render() {
