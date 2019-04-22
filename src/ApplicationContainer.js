0a4cde4d9aad24cdbddae2aef4fe4cee17e9e4f7
Nikhil Sharma (Mac) <n89sharma@gmail.com>
2019-04-21 20:50:37 -0400

    Main app container justified.

--- src/ApplicationContainer.js
+++ src/ApplicationContainer.js
@@ -5,59 +5,69 @@ import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import { componentNames as components } from './Utils/Constants';
 import ComponentSelector from './ComponentSelector';
+import Grid from '@material-ui/core/Grid';
 
 class ApplicationContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      enabledComponent : components.FOOD_DIARY
+      enabledComponent: components.FOOD_DIARY
     };
   }
 
   switchToComponent = (enabledComponent) => {
-    this.setState({enabledComponent : enabledComponent});
+    this.setState({ enabledComponent: enabledComponent });
   }
 
   render() {
     return (
       <div>
-        <AppBar position="static">
-          <Toolbar variant="dense">
+        <Grid container justify="center">
+          <Grid item xl={1}>
+            <AppBar position="static">
+              <Toolbar variant="dense">
 
-            <Button color="default" onClick={() => {this.switchToComponent(components.FOOD_DIARY)}}>
-              Food Diary
-            </Button>
+                <Button onClick={() => { this.switchToComponent(components.FOOD_DIARY) }}>
+                  Food Diary
+                </Button>
 
-            <Button color="default" onClick={() => {this.switchToComponent(components.CHECK_IN)}}>
-              Check In
-            </Button>
+                <Button onClick={() => { this.switchToComponent(components.CHECK_IN) }}>
+                  Check In
+                </Button>
 
-            <Button color="default" onClick={() => {this.switchToComponent(components.CUSTOM)}}>
-              Custom
-            </Button>
+                <Button onClick={() => { this.switchToComponent(components.CUSTOM) }}>
+                  Custom
+                </Button>
 
-            <Button color="default" onClick={() => {this.switchToComponent(components.PROGRESS)}}>
-              Progress
-            </Button>
+                <Button onClick={() => { this.switchToComponent(components.PROGRESS) }}>
+                  Progress
+                </Button>
 
-            <Button color="default" onClick={() => {this.switchToComponent(components.EXERCISE)}}>
-              Exercise
-            </Button>
+                <Button onClick={() => { this.switchToComponent(components.EXERCISE) }}>
+                  Exercise
+                </Button>
 
-            <Button color="default" onClick={() => {this.switchToComponent(components.ANALYSIS)}}>
-              Analysis
-            </Button>
+                <Button onClick={() => { this.switchToComponent(components.ANALYSIS) }}>
+                  Analysis
+                </Button>
 
-            <Button color="default" onClick={() => {this.switchToComponent(components.SETTINGS)}}>
-              Settings
-            </Button>
+                <Button onClick={() => { this.switchToComponent(components.SETTINGS) }}>
+                  Settings
+                </Button>
 
-          </Toolbar>
-        </AppBar>
+              </Toolbar>
+            </AppBar>
+          </Grid>
 
-        <ComponentSelector
-          enabledComponent={this.state.enabledComponent}
-        />
+        </Grid>
+
+        <Grid container justify="center">
+          <Grid item xl={1}>
+          </Grid>
+          <ComponentSelector
+            enabledComponent={this.state.enabledComponent}
+          />
+        </Grid>
 
       </div>
     );
